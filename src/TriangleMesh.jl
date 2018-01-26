bc407ee8d329da3ebc68289bbd98bb8991986bcf
Konrad Simon <konrad.simon@uni-hamburg.de>
2018-01-26 20:26:14 +0100

    Little change in loadpath if comiled library is there

--- src/TriangleMesh.jl
+++ src/TriangleMesh.jl
@@ -14,11 +14,11 @@ export TriMesh, Polygon_pslg,
 		set_polygon_segment!, set_polygon_segment_marker!, set_polygon_hole!,
 		write_mesh
 
-# if ~isfile(Pkg.dir() * "/TriangleMesh/deps/usr/lib/libtesselate.so")
-# 	error("Triangle library not found. Please run `Pkg.build(\"TriangleMesh\")` first.")
-# else
-# 	push!(Libdl.DL_LOAD_PATH, Pkg.dir() * "/TriangleMesh/deps/usr/lib");
-# end
+if ~isfile(Pkg.dir() * "/TriangleMesh/deps/usr/lib/libtesselate.so")
+	error("Triangle library not found. Please run `Pkg.build(\"TriangleMesh\")` first.")
+else
+	push!(Libdl.DL_LOAD_PATH, Pkg.dir() * "/TriangleMesh/deps/usr/lib");
+end
 
 # --------------------------------------
 # Contains Polygon struct
